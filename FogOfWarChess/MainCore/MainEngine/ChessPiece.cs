13338e36400537788e81f3d9e81835b5c81c8b37
AndriyYavkin <yavkinandrey@gmail.com>
2024-09-14 12:28:41 +0300

    Added new class PlayerExtensions, that will add a bit of logic(if you are white - your enemy is black )

--- FogOfWarChess/MainCore/MainEngine/ChessPiece.cs
+++ FogOfWarChess/MainCore/MainEngine/ChessPiece.cs
@@ -5,8 +5,21 @@ namespace FogOfWarChess.MainCore.MainEngine
 
     public enum Color
     {
+        None, 
         White,
         Black
+    }   
+    public static class PlayerExtensions
+    {
+        public static Color Oponent(this Color player)
+        {
+            return player switch
+            {
+                Color.White => Color.Black,
+                Color.Black => Color.White,
+                _ => Color.None,
+            };
+        }
     }
 
 
