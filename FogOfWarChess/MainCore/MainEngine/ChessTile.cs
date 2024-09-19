9688aaff554425c6716d8a41e2b90da0742e6d9d
AndriyYavkin <yavkinandrey@gmail.com>
2024-09-19 18:16:08 +0300

    Small fix

--- FogOfWarChess/MainCore/MainEngine/ChessTile.cs
+++ FogOfWarChess/MainCore/MainEngine/ChessTile.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using FogOfWarChess.MainCore.MainEngine.ChessPieces;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -12,6 +13,7 @@ public class ChessTile
 
     private Texture2D darkTileTexture2D;
     private Texture2D lightTileTexture2D;
+    //private Texture2D pawnBlack2D;
     public ChessTile(int row, int column, IChessPiece chessPiece = null)
     {
         this.row = row;
@@ -31,17 +33,17 @@ public class ChessTile
     {
         darkTileTexture2D = content.Load<Texture2D>("DarkTile");
         lightTileTexture2D = content.Load<Texture2D>("LightTile");
+        //pawnBlack2D = content.Load<Texture2D>("PawnBlack");
     }
 
     public void Draw(SpriteBatch spriteBatch, Vector2 position)
     {
-
         Texture2D tileTexture = (row + column) % 2 == 0 ? lightTileTexture2D : darkTileTexture2D;
         spriteBatch.Draw(tileTexture, position, Microsoft.Xna.Framework.Color.White);
 
         if (chessPiece != null)
         {
-            //chessPiece.Draw(spriteBatch, position); 
+            //chessPiece.Draw(spriteBatch, position);
         }
     }
 }
