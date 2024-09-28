d40324ef2d7d1dacfbd7f909c5ee2d3eabad48ea
AndriyYavkin <yavkinandrey@gmail.com>
2024-09-28 12:07:53 +0300

    Added background music

--- FogOfWarChess/MainCore/Game1.cs
+++ FogOfWarChess/MainCore/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 using Color = Microsoft.Xna.Framework.Color;
 
 namespace FogOfWarChess;
@@ -11,6 +12,8 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
     private ChessBoard chessBoard;
+    private Song song;
+
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -28,21 +31,33 @@ public class Game1 : Game
 
     protected void WindowSize(int sizeOfBoard)
     {
-        /*_graphics.PreferredBackBufferWidth =  sizeOfBoard * 40;
+        _graphics.PreferredBackBufferWidth =  sizeOfBoard * 40;
         _graphics.PreferredBackBufferHeight = sizeOfBoard * 40;
-        _graphics.ApplyChanges();*/
+        _graphics.ApplyChanges();
 
         //Or we can set it fullscreen. In this case, return boardsize protection back to private. Also if will use it, we will have standard blue background
-        _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+        /*_graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
         _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
-        _graphics.ApplyChanges();
+        _graphics.ApplyChanges();*/
         
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        this.song = Content.Load<Song>("BackgroundMusic");
+        MediaPlayer.Play(song);
+        MediaPlayer.Volume = 0.5f;
+        MediaPlayer.IsRepeating = true;
         chessBoard.LoadTexture(Content);
+    }
+
+    void MediaPlayer_MediaStateChanged(object sender, System.EventArgs e)
+    {
+        // 0.0f is silent, 1.0f is full volume
+        MediaPlayer.Volume -= 0.1f;
+        MediaPlayer.Play(song);
     }
 
     protected override void Update(GameTime gameTime)
