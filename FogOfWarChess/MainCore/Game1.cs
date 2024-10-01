4824cd997a3936640f1aaff1c271e974585b87be
AndriyYavkin <yavkinandrey@gmail.com>
2024-10-01 23:18:44 +0300

    Camera rotation by 180 degrees with button B and W to revert changes

--- FogOfWarChess/MainCore/Game1.cs
+++ FogOfWarChess/MainCore/Game1.cs
@@ -1,4 +1,6 @@
-﻿using FogOfWarChess.MainCore.MainEngine;
+﻿
+using System.ComponentModel;
+using FogOfWarChess.MainCore.MainEngine;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -13,10 +15,7 @@ public class Game1 : Game
     private SpriteBatch _spriteBatch;
     private ChessBoard chessBoard;
     private Song song;
-    Vector2 playerPoz;
-
-    Camera2D cam_white_player = new Camera2D(),
-             cam_black_player = new Camera2D();
+    Camera camera;
 
     public Game1()
     {
@@ -28,15 +27,22 @@ public class Game1 : Game
     protected override void Initialize()
     {
         chessBoard = new ChessBoard();
-        WindowSize(chessBoard.boardSize);
+        WindowSizeFind(chessBoard.boardSize);
 
         base.Initialize();
     }
 
-    protected void WindowSize(int sizeOfBoard)
+    protected void WindowSizeFind(int sizeOfBoard)
     {
-        _graphics.PreferredBackBufferWidth =  sizeOfBoard * 40;
-        _graphics.PreferredBackBufferHeight = sizeOfBoard * 40;
+        int Width = sizeOfBoard * 40;
+        int Height = sizeOfBoard * 40;
+        WindowSizeApply(Width);
+    }
+
+    protected void WindowSizeApply(int Width)
+    {
+        _graphics.PreferredBackBufferWidth =  Width;
+        _graphics.PreferredBackBufferHeight = Width;
         _graphics.ApplyChanges();
 
         //Or we can set it fullscreen. In this case, return boardsize protection back to private. Also if will use it, we will have standard blue background
@@ -55,62 +61,46 @@ public class Game1 : Game
         MediaPlayer.Volume = 0.5f;
         MediaPlayer.IsRepeating = true;
         chessBoard.LoadTexture(Content);
+        camera = new Camera(GraphicsDevice.Viewport);
     }
 
-    void MediaPlayer_MediaStateChanged(object sender, System.EventArgs e)
+    void MediaPlayer_MediaStateChanged(float LouderOrNot)
     {
         // 0.0f is silent, 1.0f is full volume
-        MediaPlayer.Volume -= 0.1f;
-        MediaPlayer.Play(song);
+        MediaPlayer.Volume -= LouderOrNot;
     }
 
     protected override void Update(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-
         // TODO: Add your update logic here
-
+        //Test for rotating camera
         if (Keyboard.GetState().IsKeyDown(Keys.W))
-            cam_white_player.Rotation = 0.5f;
+            camera.Rotation = 0;
         if (Keyboard.GetState().IsKeyDown(Keys.B))
-            cam_black_player.Rotation = 0f;
+            camera.Rotation = 21.99f;
+        // 11f for vertical fight(i don't know how explain it. just test it)
+        // 21.99f (not 22f!!!) for rotating camera by 180*
+        camera.Update(new Vector2 (_graphics.PreferredBackBufferWidth / 2,_graphics.PreferredBackBufferHeight / 2));
+        //End test for rotating camera
+        //Music Control
+        if (Keyboard.GetState().IsKeyDown(Keys.OemPlus))
+            MediaPlayer_MediaStateChanged(0.02f);
+        if (Keyboard.GetState().IsKeyDown(Keys.OemMinus))
+            MediaPlayer_MediaStateChanged(-0.02f);
+        //End of Music Control
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
-        _spriteBatch.Begin();
+        _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, null, null, null, null, camera.Transform);
         chessBoard.Draw(_spriteBatch);
         _spriteBatch.End();
         // TODO: Add your drawing code here
 
-        //CAMERA
-        //WHITE PLAYER
-        /*_spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, null, null, null, null,
-        cam_white_player.get_transformation(GraphicsDevice));
-        //_spriteBatch.Draw(texture, new Vector2(180, 130), Color.Yellow);
-        _spriteBatch.End();
-        //BLACK PLAYER
-        _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, null, null, null, null,
-        cam_black_player.get_transformation(GraphicsDevice ));
-        //_spriteBatch.Draw(chessBoard.LoadContent(Content), new Vector2(180, 130), Color.Yellow);
-        _spriteBatch.End();*/
-        //CAMERA
         base.Draw(gameTime);
-    }
-
-    protected void CameraIni()
-    {
-        playerPoz = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
-
-        cam_white_player.Pos = playerPoz;
-        cam_white_player.Zoom = 1;
-        cam_white_player.Rotation = 0;
-
-        cam_black_player.Pos = cam_white_player.Pos;
-        cam_black_player.Zoom = 1;
-        cam_black_player.Rotation = 0.5f;
     }
 }
